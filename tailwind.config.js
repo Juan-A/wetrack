9c418531fad14bc81ac0fad277b993e2440b31bb
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-05-10 08:43:13 +0200

    Landing style. Logo is pending (we need to use the vectorized one).

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                hero: ['Kumbh', ...defaultTheme.fontFamily.sans],
             },
         },
     },
