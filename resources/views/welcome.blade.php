7209e4f7cbe87bbf746df32767f8645abe39c7d6
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-01 12:48:54 +0200

    Added top commented to welcome page view.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,32 +35,62 @@
                 </span>
             </div>
         </header>
-        <div id="trends"
-            class=" mt-24 pt-5 pb-5 mb-10 self-center bg-opacity-25 bg-slate-200 rounded-xl flex flex-col flex-wrap w-11/12 lg:w-2/5">
-            <div class="flex ml-9">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    stroke="currentColor" class="w-6 h-6">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                        d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
-                </svg>
-                <span class="text-xl ml-1 font-bold"> Últimas tendencias</span>
+        <div class="flex justify-around flex-wrap">
+            <div id="trends"
+                class=" mt-24 pt-5 pb-5 mb-10 self-center bg-opacity-25 bg-slate-200 rounded-xl flex flex-col flex-wrap w-11/12 lg:w-2/5">
+                <div class="flex ml-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
+                    </svg>
+                    <span class="text-xl ml-1 font-bold"> Últimas tendencias</span>
+                </div>
+                <div class="flex justify-around flex-wrap w-11/12 lg:w-11/12 self-center">
+                    <!--Tracks-->
+                    @foreach ($trends as $track)
+                        <div class="flex flex-col w-2/5 md:max-w-40 lg:max-w-44 mr-1">
+                            <div class="flex justify-center mt-3"><img src={{ $track['image300'] }}
+                                    class="w-full rounded-lg"></div>
+                            <span class="font-bold text-lg">{{ $track['name'] }}</span>
+                            <span>
+                                <ul class="">
+                                    @foreach ($track['artists'] as $artist)
+                                        <li>{{ $artist['name'] }}</li>
+                                    @endforeach
+                                    <ul>
+                            </span>
+                        </div>
+                    @endforeach
+                </div>
             </div>
-            <div class="flex justify-around flex-wrap w-11/12 lg:w-11/12 self-center">
-                <!--Tracks-->
-                @foreach ($trends as $track)
-                    <div class="flex flex-col w-2/5 md:max-w-40 lg:max-w-44 mr-1">
-                        <div class="flex justify-center mt-3"><img src={{ $track['image300'] }}
-                                class="w-full rounded-lg"></div>
-                        <span class="font-bold text-lg">{{ $track['name'] }}</span>
-                        <span>
-                            <ul class="">
-                                @foreach ($track['artists'] as $artist)
-                                    <li>{{ $artist['name'] }}</li>
-                                @endforeach
-                                <ul>
-                        </span>
-                    </div>
-                @endforeach
+            <div id="topCommented"
+                class=" mt-5 pt-5 pb-5 mb-10 self-center bg-opacity-25 bg-slate-200 rounded-xl flex flex-col flex-wrap w-11/12 lg:w-2/5">
+                <div class="flex ml-9">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
+                      </svg>                      
+                    <span class="text-xl ml-1 font-bold"> Más comentadas</span>
+                </div>
+                <div class="flex justify-around flex-wrap w-11/12 lg:w-11/12 self-center">
+                    <!--Tracks-->
+                    @foreach ($topCommented as $reviewed)
+                        <div class="flex flex-col w-2/5 md:max-w-40 lg:max-w-44 mr-1">
+                            <div class="flex justify-center mt-3"><img src={{ $reviewed->track->json['album']['images'][1]['url'] }}
+                                    class="w-full rounded-lg"></div>
+                            <span class="font-bold text-lg">{{ $reviewed->track->name }}</span>
+                            <span>
+                                Con una nota de {{ $reviewed->calification }}
+                            </span>
+                            <span> Basada en @if($reviewed->number_of_reviews > 1)
+                                {{ $reviewed->number_of_reviews }} calificaciones.
+                                @else
+                                {{ $reviewed->number_of_reviews }} calificación.
+                                @endif
+                                
+                        </div>
+                    @endforeach
+                </div>
             </div>
         </div>
     </div>
