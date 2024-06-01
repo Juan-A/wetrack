fd7b16e2c39577b21bcd0e717b5067bf21593528
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-01 12:09:40 +0200

    Added welcome page trends number to .env

--- config/spotify.php
+++ config/spotify.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Str;
 return [
     'clientId' => env('SPOTIFY_CLIENT_ID'),
     'clientSecret' => env('SPOTIFY_CLIENT_SECRET'),
-    'resultsPerPage' => env('PER_PAGE_RESULTS')
+    'resultsPerPage' => env('PER_PAGE_RESULTS'),
+    'trendsNumber' => env("TRENDS_NUMBER"),
 ];
