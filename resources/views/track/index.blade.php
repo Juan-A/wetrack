6d3fa1976582308b92394d252cc822b7cb23dff4
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-04 22:36:39 +0200

    Fixed modal.

--- resources/views/track/index.blade.php
+++ resources/views/track/index.blade.php
@@ -213,8 +213,6 @@
 
                                 <span class="ml-1">{{ $review['user']['name'] }}</span>
                                 @can('delete', $review)
-                                        @method('delete')
-                                        @csrf
                                         <button class="btn btn-circle btn-outline btn-sm mr-4 ml-5" x-data=""
                                         x-on:click.prevent="$dispatch('open-modal', 'confirm-review-deletion')">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -223,6 +221,23 @@
                                                     d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
                                         </button>
+                                        <x-modal name="confirm-review-deletion" focusable>
+                                            <form action="{{ route('review.delete', $review) }}" method="POST" class="p-6">
+                                                @csrf
+                                                @method('delete')
+                                    
+                                                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                    {{ __('¿Seguro que quieres eliminar tu reseña?') }}
+                                                </h2>
+                                    
+                                                <div class="mt-6 flex justify-end">
+                                                    <button x-on:click.prevent="$dispatch('close')" onclick="event.preventDefault()"
+                                                        class="btn btn-active btn-ghost dark:text-white">CANCELAR</button>
+                                    
+                                                    <button class="btn btn-error ml-4">ELIMINAR RESEÑA</button>
+                                                </div>
+                                            </form>
+                                        </x-modal>
                                 @endcan
                             </div>
                             {{ $review['review'] }}
@@ -234,23 +249,7 @@
 
     </div>
     </div>
-    <x-modal name="confirm-review-deletion" focusable>
-        <form action="{{ route('review.delete', $review) }}" method="POST" class="p-6">
-            @csrf
-            @method('delete')
 
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
-                {{ __('¿Seguro que quieres eliminar tu reseña?') }}
-            </h2>
-
-            <div class="mt-6 flex justify-end">
-                <button x-on:click.prevent="$dispatch('close')" onclick="event.preventDefault()"
-                    class="btn btn-active btn-ghost dark:text-white">CANCELAR</button>
-
-                <button class="btn btn-error ml-4">ELIMINAR RESEÑA</button>
-            </div>
-        </form>
-    </x-modal>
     @include('layouts.footer')
 </body>
 
