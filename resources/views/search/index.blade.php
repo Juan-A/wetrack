63e2e288b8b0046358eb8d510f5369d442df560e
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-04 22:25:42 +0200

    Title update.

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@100..900&display=swap" rel="stylesheet">
