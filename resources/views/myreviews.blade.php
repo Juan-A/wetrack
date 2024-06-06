42f8b2795d8a8a61b34a942e80ecebb24a6519a7
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-06 09:03:19 +0200

    Added modal instead of js alert to confirm the review deletion.

--- resources/views/myreviews.blade.php
+++ resources/views/myreviews.blade.php
@@ -41,20 +41,32 @@
                                         <div class="ml-auto flex items-center">
 
                                         </div>
-                                        <form action="{{ route('review.delete', $review) }}" method="POST"
-                                            class="p-6">
-                                            @csrf
-                                            @method('delete')
-                                            <button class="btn btn-circle btn-outline btn-md mr-4"
-                                                onclick="confirmAction(this)">
-                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none"
-                                                    viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                    class="size-6">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                                </svg>
-                                            </button>
-                                        </form>
+                                        <label for="delete_{{$review->id}}" class="btn btn-circle btn-outline btn-sm mr-4 ml-4">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                            </svg>
+                                        </label>
+                                        <input type="checkbox" id="delete_{{$review->id}}" class="modal-toggle" />
+                                    <div class="modal modal-bottom sm:modal-middle" role="dialog">
+                                        <div class="modal-box">
+                                            <form action="{{ route('review.delete', $review) }}" method="POST"
+                                                class="p-6">
+                                                @csrf
+                                                @method('delete')
+
+                                                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                    {{ __('¿Seguro que quieres eliminar tu reseña?') }}
+                                                </h2>
+
+                                                <div class="mt-6 flex justify-end">
+                                                    <label for="delete_{{$review->id}}" class="btn">CANCELAR</label>
+                                                    <button class="btn btn-error ml-4">ELIMINAR RESEÑA</button>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
                                     </div>
                                 </a>
                             @endforeach
@@ -67,17 +79,4 @@
             </div>
         </div>
     </div>
-    <script>
-        function confirmAction(button) {
-            event.preventDefault(); // Prevent default form submission
-            var confirmation = confirm("¿Estás seguro de que deseas eliminar la reseña?");
-            var form = button.closest("form");
-            if (confirmation) {
-                form.submit();
-            } else {
-
-            }
-
-        }
-    </script>
 </x-app-layout>
