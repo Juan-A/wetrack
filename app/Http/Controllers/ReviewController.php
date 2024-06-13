bf8cb72cec42871c3cc5a2cc1a7fbccc192d5b6f
Juan Antonio Herrero Centurion <juan13herrero@gmail.com>
2024-06-13 21:25:16 +0200

    Code refactor.

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -13,20 +13,6 @@ use Illuminate\Validation\Rules\Exists;
 
 class ReviewController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(Track $track, Request $request)
-    {
-    }
     public function getReviews($spotifyTrackId)
     {
         return Review::with('user')->where('spotify_id', '=', $spotifyTrackId);
@@ -112,34 +98,12 @@ class ReviewController extends Controller
 
         return round($averageRating, 2);
     }
-    /**
-     * Display the specified resource.
-     */
-    public function show(Review $review)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Review $review)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Review $review)
-    {
-        //
-    }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Review $review, User $user):RedirectResponse
+    public function destroy(Review $review):RedirectResponse
     {
         Gate::authorize('delete',$review);
         $review->delete();
